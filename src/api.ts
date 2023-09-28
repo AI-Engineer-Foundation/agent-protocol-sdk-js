256a9f20096a06a6491e607be81d0289398dcf5b
dOrgJelli <jelli@dorg.tech>
2023-09-28 20:36:07 +0200

    chore: update typings

---
+++ src/api.ts
@@ -0,0 +1,52 @@
+import * as OpenApiValidator from 'express-openapi-validator'
+import yaml from 'js-yaml'
+import express from 'express'
+import * as core from 'express-serve-static-core';
+
+import spec from '../../../schemas/openapi.yml'
+
+export type ApiApp = core.Express;
+
+export interface RouteContext {
+  workspace: string;
+}
+
+export type RouteRegisterFn = (
+  app: ApiApp,
+  context: RouteContext
+) => ApiApp;
+
+export interface ApiConfig {
+  context: RouteContext;
+  port: number;
+  callback?: () => void;
+  routes: RouteRegisterFn[];
+}
+
+export const createApi = (config: ApiConfig) => {
+  const app = express()
+
+  app.use(express.json())
+  app.use(express.text())
+  app.use(express.urlencoded({ extended: false }))
+
+  const parsedSpec = yaml.load(spec)
+
+  app.use(
+    OpenApiValidator.middleware({
+      apiSpec: parsedSpec as any,
+      validateRequests: true, // (default)
+      validateResponses: true, // false by default
+    })
+  )
+
+  app.get('/openapi.yaml', (_, res) => {
+    res.setHeader('Content-Type', 'text/yaml').status(200).send(spec)
+  })
+
+  for (const route of config.routes) {
+    route(app, config.context);
+  }
+
+  app.listen(config.port, config.callback)
+}
